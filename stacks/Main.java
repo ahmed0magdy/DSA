551d6b0c7726b0860bc27d8bdeffedc44f3771a8
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2024-04-18 14:45:35 +0200

    reversed string

---
+++ stacks/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = null;
+        StringReverser reverser = new StringReverser();
+        var rev = reverser.reverse(str);
+        System.out.println(rev);
+    }
+}
