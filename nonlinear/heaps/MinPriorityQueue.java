e2bb0d8aa557bfb6732e665b0d6e7950c7727ea8
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2024-07-01 12:22:57 +0300

    heap

--- nonlinear/heaps/MinPriorityQueue.java
+++ nonlinear/heaps/MinPriorityQueue.java
@@ -2,16 +2,31 @@ package nonlinear.heaps;
 
 public class MinPriorityQueue {
 
+    private MinHeap minHeap;
+
+    public MinPriorityQueue(int capacity) {
+        minHeap = new MinHeap(capacity);
+    }
+
+    public void add(String value, int priority) {
+        minHeap.insert(priority, value);
+    }
+
+    public String remove() {
+        return minHeap.remove();
+    }
+
+    public boolean isEmpty() {
+        return minHeap.isEmpty();
+    }
+
 }
-
-
 
 // Implement a min priority queue with the following operations: 
 // • add(String value, int priority)
 // • remove()
 // • isEmpty()
 // Use the MinHeap class that 
-
 // you created in the last exercise. Items with 
 // a smaller priority should be moved to the beginning of the queue. Hint: 
 // use the priority of each item as the key in your min heap.
