faf4e6dc7668e502dc0f18777dc9fda02566434e
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2024-07-16 18:50:26 +0300

    Wgraphs

--- nonlinear/Graphs/Main.java
+++ nonlinear/Graphs/Main.java
@@ -2,20 +2,52 @@ package nonlinear.Graphs;
 
 public class Main {
     public static void main(String[] names) {
-        var graph = new Graph();
-        graph.addNode("A");
-        graph.addNode("B");
-        graph.addNode("C");
-        graph.addEdge("A", "B");
-        graph.addEdge("B", "C");
-        graph.addEdge("C", "A");
+        // var graph = new Graph();
+        // graph.addNode("A");
+        // graph.addNode("B");
+        // graph.addNode("C");
+        // graph.addEdge("A", "B");
+        // graph.addEdge("B", "C");
+        // graph.addEdge("C", "A");
 
-        // graph.removeEdge("A", "c");
-        // graph.removeNode("A");
-        // graph.print();
-        // graph.traverseBreadthFirst("K");
-        // graph.traverseDepthFirst("K");
-        // System.out.println(graph.topologicalSort());
-        System.out.println(graph.hasCycle());
+        // // graph.removeEdge("A", "c");
+        // // graph.removeNode("A");
+        // // graph.print();
+        // // graph.traverseBreadthFirst("K");
+        // // graph.traverseDepthFirst("K");
+        // // System.out.println(graph.topologicalSort());
+        // System.out.println(graph.hasCycle());
+
+        // var graph = new WGraph();
+        // graph.addNode("A");
+        // graph.addNode("B");
+        // graph.addNode("C");
+        // graph.addEdge("A", "B", 1);
+        // graph.addEdge("B", "C", 2);
+        // graph.addEdge("A", "C", 10);
+        // System.out.println(graph.getShortestPath("A", "C"));
+
+        // var graph = new WGraph();
+        // graph.addNode("A");
+        // graph.addNode("B");
+        // graph.addNode("C");
+        // graph.addEdge("A", "B", 0);
+        // graph.addEdge("B", "C", 0);
+        // graph.addEdge("C", "A", 0);
+        // System.out.println(graph.hasCycle());
+
+        // var graph = new WGraph();
+        // graph.addNode("A");
+        // graph.addNode("B");
+        // graph.addNode("C");
+        // graph.addNode("D");
+        // graph.addEdge("A", "B", 3);
+        // graph.addEdge("B", "D", 4);
+        // graph.addEdge("C", "D", 5);
+        // graph.addEdge("A", "C", 1);
+        // graph.addEdge("B", "C", 2);
+        // var tree = graph.getMinSpanningTree();
+        // tree.print();
+
     }
 }
