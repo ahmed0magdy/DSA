37add92801ef0c3bcaaa36920940a42c7ef912b5
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2025-01-13 09:13:22 +0200

    27. Remove Element

--- leetcode/Main.java
+++ leetcode/Main.java
@@ -3,27 +3,24 @@ package leetcode;
 public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        // int[] nums = { 1, 1, 2 };
-        int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
-        int result = sol.removeDuplicates(nums);
+        // int[] nums = { 3, 2, 2, 3 };
+        // int val = 3;
+        int[] nums = { 0, 1, 2, 2, 3, 0, 4, 2 };
+        int val = 2;
+        var result = sol.removeElement(nums, val);
         System.out.println(result);
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
-
-        }
-
+        for(int i=0;i<nums.length;i++)
+        System.out.println(nums[i]);
     }
 }
 
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        int count = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i - 1]) {
-                nums[count] = nums[i];
-                count++;
-            }
+    public int removeElement(int[] nums, int val) {
+        int p = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val)
+                nums[p++] = nums[i];
         }
-        return count;
+        return p;
     }
 }
