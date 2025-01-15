a18658a2d340159a136a7656b6f0c358ed1cc3c3
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2025-01-15 13:43:20 +0200

    155. Min Stack- two stacks sol

--- leetcode/Main.java
+++ leetcode/Main.java
@@ -6,31 +6,57 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        var sol = new Solution();
-        String[] s = { "[(])", "([{}])", "(]", "[]" };
-        for (String w : s)
-            System.out.println(sol.isValid(w));
-
+        MinStack obj = new MinStack();
+        obj.push(-2);
+        obj.push(0);
+        obj.push(-3);
+        obj.pop();
+        int param_3 = obj.top();
+        int param_4 = obj.getMin();
+        System.out.println(param_3);
+        System.out.println(param_4);
     }
 }
 
-class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        Map<Character, Character> CloseOpen = new HashMap<>();
-        CloseOpen.put(']', '[');
-        CloseOpen.put(')', '(');
-        CloseOpen.put('}', '{');
+class MinStack {
 
-        for (char c : s.toCharArray()) {
-            if (CloseOpen.containsKey(c)) {
-                if (!stack.isEmpty() && stack.peek() == CloseOpen.get(c))
-                    stack.pop();
-                else
-                    return false;
-            } else
-                stack.push(c);
-        }
-        return stack.isEmpty();
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+
+    public MinStack() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack1.push(val);
+        if (stack2.isEmpty() || stack1.peek() <= stack2.peek())
+            stack2.push(val);
+    }
+
+    public void pop() {
+        if (stack1.isEmpty())
+            return;
+        int top = stack1.pop();
+        if (top == stack2.peek())
+            stack2.pop();
+
+    }
+
+    public int top() {
+        return stack1.peek();
+    }
+
+    public int getMin() {
+        return stack2.peek();
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
